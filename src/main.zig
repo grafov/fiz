137f50378e17ceb5dc75ecd0a28860657928b1ae
Alexander I.Grafov <grafov@inet.name>
2024-05-18 22:11:32 +0300

    Naive draft withot error catching

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,22 @@
 const std = @import("std");
+const allocator = std.debug.global_allocator;
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // don't forget to flush!
+    //    var args = std.process.args();
+    const args = std.os.argv;
+    if (args.len <= 1) {
+        return;
+    }
+    var dir = std.fs.openDirAbsoluteZ(args[1], .{ .iterate = true }) catch unreachable;
+    var iterator = dir.iterate();
+    while (try iterator.next()) |it| {
+        try stdout.print("{s}\n", .{it.name});
+    }
+    try bw.flush();
 }
 
 test "simple test" {
