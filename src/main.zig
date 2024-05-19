9643e45f43e1a370dfd1e60a4b280d9e86817cda
Alexander I.Grafov <grafov@inet.name>
2024-05-19 18:27:40 +0300

    Partially handle directory opening errors

--- src/main.zig
+++ src/main.zig
@@ -3,20 +3,51 @@ const allocator = std.debug.global_allocator;
 
 pub fn main() !void {
     const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    const stderr_file = std.io.getStdErr().writer();
+    var sw = std.io.bufferedWriter(stdout_file);
+    var ew = std.io.bufferedWriter(stderr_file);
+    const stderr = ew.writer();
+    const stdout = sw.writer();
+    defer ew.flush() catch unreachable;
+    defer sw.flush() catch unreachable;
 
     //    var args = std.process.args();
     const args = std.os.argv;
     if (args.len <= 1) {
+        // тут брать cwd
         return;
     }
-    var dir = std.fs.openDirAbsoluteZ(args[1], .{ .iterate = true }) catch unreachable;
+    var dir = std.fs.openDirAbsoluteZ(args[1], .{ .iterate = true }) catch |err| {
+        switch (err) {
+            error.NoSpaceLeft => {
+                try stderr.print("fi: no space left for '{s}'\n", .{args[1]});
+            },
+            error.FileNotFound => {
+                try stderr.print("fi: path not found for '{s}'\n", .{args[1]});
+            },
+            error.FileTooBig => {
+                try stderr.print("fi: file too big for '{s}'\n", .{args[1]});
+            },
+            error.AccessDenied => {
+                try stderr.print("fi: access denied for '{s}'\n", .{args[1]});
+            },
+            error.DeviceBusy => {
+                try stderr.print("fi: device busy for '{s}'\n", .{args[1]});
+            },
+            error.NameTooLong => {
+                try stderr.print("fi: name too long for '{s}'\n", .{args[1]});
+            },
+            // too lazy
+            else => {
+                try stderr.print("fi: can't open '{s}'\n", .{args[1]});
+            },
+        }
+        return;
+    };
     var iterator = dir.iterate();
     while (try iterator.next()) |it| {
         try stdout.print("{s}\n", .{it.name});
     }
-    try bw.flush();
 }
 
 test "simple test" {
