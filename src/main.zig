6cc2cdd54a7221a59abe9be0d4456842256bfaf0
Alexander I.Grafov <grafov@inet.name>
2024-05-21 21:08:54 +0300

    Handle relative dir and omited path
    In a rough way yet.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-const allocator = std.debug.global_allocator;
 
 const str = []const u8;
 const defaultIcon: str = " ";
@@ -7,6 +6,10 @@ const fileIcon: str = " ";
 const folderIcon: str = "🗀";
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
     const stdout_file = std.io.getStdOut().writer();
     const stderr_file = std.io.getStdErr().writer();
     var sw = std.io.bufferedWriter(stdout_file);
@@ -16,35 +19,43 @@ pub fn main() !void {
     defer ew.flush() catch unreachable;
     defer sw.flush() catch unreachable;
 
-    //    var args = std.process.args();
-    const args = std.os.argv;
-    if (args.len <= 1) {
-        // тут брать cwd
-        return;
+    var fpath: [*:0]u8 = undefined;
+    if (std.os.argv.len > 1) {
+        fpath = std.os.argv[1];
+    } else {
+        const cwd = try std.process.getEnvVarOwned(allocator, "PWD");
+        defer allocator.free(cwd);
+        fpath = try allocator.dupeZ(u8, cwd);
     }
-    var dir = std.fs.openDirAbsoluteZ(args[1], .{ .iterate = true }) catch |err| {
+    if (!std.fs.path.isAbsoluteZ(fpath)) {
+        const cwd = try std.process.getEnvVarOwned(allocator, "PWD");
+        defer allocator.free(cwd);
+        const paths = [_][]u8{ cwd, std.mem.span(fpath) };
+        fpath = try std.fs.path.joinZ(allocator, &paths);
+    }
+    var dir = std.fs.openDirAbsoluteZ(fpath, .{ .iterate = true }) catch |err| {
         switch (err) {
             error.NoSpaceLeft => {
-                try stderr.print("fi: no space left for '{s}'\n", .{args[1]});
+                try stderr.print("fi: no space left for '{s}'\n", .{fpath});
             },
             error.FileNotFound => {
-                try stderr.print("fi: path not found for '{s}'\n", .{args[1]});
+                try stderr.print("fi: path not found for '{s}'\n", .{fpath});
             },
             error.FileTooBig => {
-                try stderr.print("fi: file too big for '{s}'\n", .{args[1]});
+                try stderr.print("fi: file too big for '{s}'\n", .{fpath});
             },
             error.AccessDenied => {
-                try stderr.print("fi: access denied for '{s}'\n", .{args[1]});
+                try stderr.print("fi: access denied for '{s}'\n", .{fpath});
             },
             error.DeviceBusy => {
-                try stderr.print("fi: device busy for '{s}'\n", .{args[1]});
+                try stderr.print("fi: device busy for '{s}'\n", .{fpath});
             },
             error.NameTooLong => {
-                try stderr.print("fi: name too long for '{s}'\n", .{args[1]});
+                try stderr.print("fi: name too long for '{s}'\n", .{fpath});
             },
             // too lazy
             else => {
-                try stderr.print("fi: can't open '{s}'\n", .{args[1]});
+                try stderr.print("fi: can't open '{s}'\n", .{fpath});
             },
         }
         return;
@@ -90,13 +101,6 @@ pub fn main() !void {
     }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
 fn displayMask(m: std.fs.File.Mode) str {
     if (m == 0) {
         return ""; // amask.get(0);
@@ -114,3 +118,10 @@ var amask = std.AutoArrayHashMapUnmanaged(u8, []const u8){
     .{ 0b110, "☳" }, // rw-
     .{ 0b111, "☷" }, // rwx
 };
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
